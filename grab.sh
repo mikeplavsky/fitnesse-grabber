9777a221dd589fc459fab96a2f9a21f26b32ef42
mikplavsky <mikhail.plavskiy@quest.com>
2020-04-16 00:20:59 +0300

    skipping more

--- grab.sh
+++ grab.sh
@@ -5,8 +5,8 @@ URL=$1
 wget --page-requisites \
      --convert-links \
      --no-parent \
-     -r -l 2 -nH \
+     -r -l 3 -nH \
      --adjust-extension \
      --reject-regex \
-     '.*\?suite$|.*\?test$|.*\?whereUsed|.*remote_debug|.*purge.*|.*\?search$' \
+     '.*\?suite$|.*\?test$|.*\?whereUsed|.*remote_debug|.*purge.*|.*\?search$|.*UserGuide$|.*\?search$|.*RecentChanges$' \
      $URL
