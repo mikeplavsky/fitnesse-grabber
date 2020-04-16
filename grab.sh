5d6334415f364d28eb80755481ed9538a8431283
mikplavsky <mikhail.plavskiy@quest.com>
2020-04-16 23:25:09 +0300

    filtering by test name

--- grab.sh
+++ grab.sh
@@ -1,12 +1,15 @@
 set -euo pipefail
 
 URL=$1
+TEST=`echo $1 | awk -F. '{print $NF}'`
+
+REGEX='.*\?suite$|.*\?test$|.*\?whereUsed|.*remote_debug|.*purge.*|.*\?search$|.*UserGuide$|.*\?search$|.*RecentChanges$|.*\?refactor.*'
 
 wget --page-requisites \
      --convert-links \
      --no-parent \
      -r -l 3 -nH \
      --adjust-extension \
-     --reject-regex \
-     '.*\?suite$|.*\?test$|.*\?whereUsed|.*remote_debug|.*purge.*|.*\?search$|.*UserGuide$|.*\?search$|.*RecentChanges$' \
+     --accept-regex ".*$TEST.*" \
+     --reject-regex $REGEX \
      $URL
