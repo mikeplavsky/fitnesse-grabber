69b26621a0fc42136cfced5e91f9709ed7cd5452
mikplavsky <mikhail.plavskiy@quest.com>
2020-04-26 17:02:09 +0300

    lambda code for running ECS task

---
+++ run_lambda.py
@@ -0,0 +1,37 @@
+import boto3
+ecs = boto3.client('ecs')
+
+def lambda_main(url, dir):
+
+   ecs.run_task(
+
+       cluster="FitnesseFargate",
+       count=1,
+       launchType="FARGATE",
+
+       networkConfiguration=dict(
+           awsvpcConfiguration=dict(
+               subnets=['subnet-572af478'],
+               securityGroups=['sg-c7b433a2'],
+               assignPublicIp='ENABLED'
+           )),
+
+       taskDefinition="fitnesse-grabber:11",
+
+       overrides=dict(
+           containerOverrides=[dict(
+               name="fitnesse-grabber",
+               command=["bash","./grab.sh", url, dir])])) 
+
+if __name__ == "__main__":
+
+    from sys import argv, exit
+    print(argv)
+
+    if len(argv) < 3:
+        print(f'Need URL and DIR')
+        exit(-1)
+
+    lambda_main(argv[1], argv[2])
+
+
